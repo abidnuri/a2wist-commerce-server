c149095b754cd4be73ada849992769d6ce61c423
Ishtiak <iahmed1606@gmail.com>
2021-07-27 12:33:17 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,6 +9,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Mongodb connetion
+const DB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xzynl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+// Mongoose
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("DB connected succesfully");
+  })
+  .catch((err) => console.log("Error : ", err));
 
 // Routes
 const authRouter = require("./routes/authRoutes");
