3ff2750505f69e08502e680c788e41ca642d6ab7
Ishtiak <iahmed1606@gmail.com>
2021-07-29 16:17:56 +0600

    env port added to solve heroku deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -35,4 +37,4 @@ app.get("/ping", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(3333, console.log("server is running"));
+app.listen(process.env.PORT || 3333, console.log("server is running"));
